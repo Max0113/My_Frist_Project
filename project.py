b1e7abed448fc92b2167b989640bff4cb024f184
Max0113 <creater202316@gmail.com>
2025-06-10 14:49:44 +0100

    PORJECT FOR INDIC AGE

--- project.py
+++ project.py
@@ -1,2 +1,11 @@
-x = 20
-print ("hello" + str(x) )
+x = int(input ("what your age?"))
+if x > 18 :
+    print ("that nice you can work")
+elif x == 18 :
+    y = input ("you have a cart national? yes ou no")
+    if y == "yes" :
+       print ("that nice you can work")
+    else :
+        print ("I'am sorry but you don't can a work")
+else :
+    print ("I'am sorry but you don't can a work")
