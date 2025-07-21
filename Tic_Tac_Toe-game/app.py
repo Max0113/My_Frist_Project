eae4c27cff9af66edfd1eea25a9e72e20c6ebbab
Max0113 <creater202316@gmail.com>
2025-07-21 18:27:21 +0100

    X-o game modifcation

--- Tic_Tac_Toe-game/app.py
+++ Tic_Tac_Toe-game/app.py
@@ -1,3 +1,8 @@
+import os
+
+def clear_wind() :
+    os.system("cls")
+
 class Player :
     def __init__(self):
         self.name = ""
@@ -13,7 +18,7 @@ class Player :
 
     def choose_symbol(self) :
         while True :
-            symbol = input (f"player 1 {self.name} entre your symbol : ")
+            symbol = input (f"player {self.name} entre your symbol : ")
             if symbol.isalpha() and len(symbol) == 1 :
                self.symbol = symbol.upper()
                break
@@ -25,9 +30,8 @@ class Menu :
           welcome to my X-0 game
           1 - Start Game
           2 - Quit Game
-          entrer your choose 1 et 2 :
-    """
-        choose = input(x)
+          entrer your choose 1 et 2 : """
+        choose = int(input(x))
         return choose
     
     def End_main(self) :
@@ -36,29 +40,120 @@ class Menu :
           1 - restart Game
           2 - Quit Game
           entrer your choose 1 et 2 : """
-        choose = input(x)
+        choose = int(input(x))
         return choose
     
-class board :
+class Board :
 
     def __init__(self):
-        self.borad = [1,2,3,4,5,6,7,8,9]
+        self.borad = []
+        for i in range(1,10) :
+            self.borad.append(str(i))
     
     def display_board(self) :
-        pass
+        for i in range(0,9,3) :
+            print('|'.join(self.borad[i:i+3]))
+            if i < 6 :
+                print("-"*10)
+        
+    def updata_board(self , choice , symbol) :
+        if self.is_valid_move(choice) :
+            self.borad[choice - 1] = symbol
+            return True
+        return False
 
+    def is_valid_move(self , choice) :
+        return self.borad[choice - 1].isdigit()
     
+    def rest_board(self) :
+        self.borad = []
+        for i in range(1,10) :
+            self.borad.append(str(i))
 
-n = [1,2,3,4,5,6,7,8,9]
-j = 0
-for i in n :
-    j += 1
-    if i == j :
-       x = f"""
-        {j}|{n}|{n}
-       -------------
-        {n}|{n}|{n}
-       -------------
-        {n}|{n}|{n}
-       """
-print(x)
+class Game() :
+    def __init__(self):
+        self.player = [Player(),Player()]
+        self.board = Board()
+        self.menu = Menu()
+        self.current_player_index = 0
+
+    def start_game(self) :
+        choice = self.menu.start_main()
+        if choice == 1 :
+            self.setup_game()
+            self.play_game()
+        else :
+            self.Quit_game()
+
+    def setup_game(self) :
+        for Number , player in enumerate(self.player) :
+            print(f"Player {Number + 1} : ")
+            player.choose_name()
+            player.choose_symbol()
+            print('-'*20)
+            # clear_wind()
+
+    def play_game(self) :
+        while True :
+            self.play_turn() 
+            if self.check_win() or  self.check_draw() :
+                choise = self.menu.End_main()
+                if choise == 1 :
+                    self.rest_game()
+                else :
+                    self.Quit_game()
+                    break
+
+    def play_turn(self) :
+        player = self.player[self.current_player_index] 
+        self.board.display_board()
+        print (f" {player.name}'s start playing ({player.symbol})")
+        while True :
+            try:
+                cell_player = int(input("enter number of place (1-9) : "))
+                if  1 <= cell_player <= 9 and self.board.updata_board(cell_player , player.symbol) :
+                    break
+                else :
+                    print("enter une number enter 1-9")
+            except ValueError :
+                print("An error enter noumber entre 1-9")
+        self.switch_player() 
+        clear_wind()
+
+    def switch_player(self) :
+        self.current_player_index = 1 - self.current_player_index
+    
+    def rest_game(self) :
+        self.board.rest_board()
+        self.current_player_index = 0
+        self.play_game()
+
+    def check_win(self) :
+        b = self.board.borad
+        win_conditions = [
+        [0, 1, 2],  # الصف الأول
+        [3, 4, 5],  # الصف الثاني
+        [6, 7, 8],  # الصف الثالث
+        [0, 3, 6],  # العمود الأول
+        [1, 4, 7],  # العمود الثاني
+        [2, 5, 8],  # العمود الثالث
+        [0, 4, 8],  # القطر الرئيسي
+        [2, 4, 6],  # القطر الثانوي
+        ]
+        for comba in win_conditions :
+            a , b1 , c = comba
+            if b[a] == b[b1] == b[c] and not b[a].isdigit() :
+                self.board.display_board()
+                return True
+        return False
+
+    def check_draw(self) :
+        return all(not cell.isdigit() for cell in self.board.borad )
+
+
+    def Quit_game(self) :
+        print (" thnik you for playing ??")
+        quit()
+    
+game = Game()
+game.start_game()
